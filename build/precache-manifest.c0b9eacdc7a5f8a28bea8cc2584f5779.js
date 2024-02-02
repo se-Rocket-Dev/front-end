2d773acdf67f88ed224899133b1e7ea0867b603a
PhonlapatS <PhonlapatS.2002@gmail.com>
2024-02-02 13:53:34 +0700

    add build directory 2

---
+++ build/precache-manifest.c0b9eacdc7a5f8a28bea8cc2584f5779.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3faf1b7f11c45344df86098e025885b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80e2feadc96181329bef",
+    "url": "/static/css/2.7f1403ef.chunk.css"
+  },
+  {
+    "revision": "bf6f22e4d059cf14ca00",
+    "url": "/static/css/main.61582e14.chunk.css"
+  },
+  {
+    "revision": "80e2feadc96181329bef",
+    "url": "/static/js/2.9810f920.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.9810f920.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6f22e4d059cf14ca00",
+    "url": "/static/js/main.15c42095.chunk.js"
+  },
+  {
+    "revision": "35f67668c217d61057f5",
+    "url": "/static/js/runtime-main.ed9d8f35.js"
+  }
+]);
